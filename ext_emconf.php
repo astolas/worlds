72502d149ba93bb1314ea96581167367e004d4ed
Erik Konrad <ek@illusionforge.info>
2020-09-08 20:17:08 +0200

    Initial import

---
+++ ext_emconf.php
@@ -0,0 +1,37 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "news".
+ *
+ * Auto generated 08-09-2020 19:48
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'T3Worlds',
+  'description' => 'TYPO3 Extension for managing and rendering WebGL and Meshcontent in TYPO3 and building virtual worlds',
+  'category' => 'fe',
+  'author' => 'Erik Konrad',
+  'author_email' => 'e_konrad84@yahoo.de',
+  'state' => 'alpha',
+  'clearCacheOnLoad' => true,
+  'version' => '1.0.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '9.5.17-10.9.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+
+  ),
+  'uploadfolder' => false,
+  'clearcacheonload' => true,
+  'author_company' => NULL,
+);
+
